298df372fc5346ae4385f5c8d12cc605cf855ea2
Brian Cox <briancox730@gmail.com>
2014-06-08 12:14:13 -0400

    all passing test

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,16 @@
 class BoundingArea
+
+  def initialize(bounding_boxes)
+    @boxes = bounding_boxes
+
+  end
+
+  def contains_point?(point_x, point_y)
+    @boxes.each do |box|
+      if box.contains_point?(point_x, point_y)
+        return true
+      end
+    end
+    false
+  end
 end
--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,41 @@
 class BoundingBox
+
+  def initialize(x, y, width, height)
+    @x = x
+    @y = y
+    @width = width
+    @height = height
+  end
+
+  def width
+    @width
+  end
+
+  def height
+    @height
+  end
+
+  def left
+    @x
+  end
+
+  def right
+    @x + @width
+  end
+
+  def top
+    @y + @height
+  end
+
+  def bottom
+    @y
+  end
+
+  def contains_point?(point_x, point_y)
+    if point_x >= @x && point_x <= @x + @width && point_y >= @y && point_y <= @y + @height
+      true
+    else
+      false
+    end
+  end
 end
